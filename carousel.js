e835bb768e4c4c9d6a2e34ba2b07a7d74b66e701
priyeshj1 <136233447+priyeshj1@users.noreply.github.com>
2023-06-11 22:57:21 +0530

    v1.0.1

---
+++ carousel.js
@@ -0,0 +1,6 @@
+const track = document.querySelector('.carousel_track');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('.right');
+const prevButton = document.querySelector('.left');
+const dotsNav = document.querySelector('carousel_nav');
+const dots = Array.from(dotsNav.childern);
